f936b73f37d56c2603b8098557a4967ed2fa1cf4
Raya2399 <102332034+Raya2399@users.noreply.github.com>
2023-03-24 11:59:34 +0800

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
 const chalk = require('chalk')
 // EDIT DISINI
-global.owner = ['6282127075205'] // no own
-global.author = 'KurrXd' // nama author 
-global.packname = 'xd bot whatsapp' // nama pack sticker
-global.namabot = 'XD BOTZ' // nama bot mu
-global.group = 'https://bit.ly/xdbotwhatsapp' // grup mu
-global.pic = 'https://telegra.ph/file/36211850fa46193ef96ad.jpg' // logo lu
+global.owner = ['6282261065144'] // no own
+global.author = 'Fin Ofc' // nama author 
+global.packname = 'FIN MD bot whatsapp' // nama pack sticker
+global.namabot = 'FIN MD' // nama bot mu
+global.group = 'https://chat.whatsapp.com/JaDhlN6irs0AbP2LuIDmlH' // grup mu
+global.pic = 'https://telegra.ph/file/357fcef6b4cd35df96569.jpg' // logo lu
 
 
 let file = require.resolve(__filename)
